e234c2ca531e77b6bf3c21c169535cb2470c4ff9
Ian Engelbrecht <ianicus.za@gmail.com>
2023-09-11 13:01:05 +0200

    recursive dbf extract

--- dbf_extract.py
+++ dbf_extract.py
@@ -1,4 +1,4 @@
-from os import path, listdir, remove, mkdir
+from os import path, walk, listdir, remove, mkdir
 from dbfread import DBF, FieldParser
 import csv
 import re
@@ -7,33 +7,34 @@ import re
 from customParsers import stringdates
 
 #first get the csv files from BRAHMS dbf files
-dbdir = r'D:\BACKUP BRAHMS_31 08 2021\BRAHMS_KNP_DATA\myrdefiles' #The BRAHMS database folder
-outputdir = r'C:\temp\SkukuzaBRAHMS/rde'
+dbdir = r'D:\NSCF Data WG\Data\Moss\Moss Herbarium Database\NSCF Data capture' #The BRAHMS database folder
+outputdir = r'D:\NSCF Data WG\Data\Moss\Moss Herbarium Database\csv'
 
 if not path.isdir(outputdir):
   mkdir(outputdir)
 
-dbfs = [f for f in listdir(dbdir) if path.isfile(path.join(dbdir, f)) and f.lower().endswith('dbf')]
+for root, dir_names, file_names in walk(dbdir):
+  for f in file_names:
+    if f.endswith('.dbf'):
+      dbf = path.join(root, f)
+      recordCount = 0
+      csvfile = re.sub('\.dbf$', '.csv', f, flags = re.I)
+      with open(path.join(outputdir, csvfile), 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        fieldsadded = False
+        for record in DBF(dbf, char_decode_errors='ignore', parserclass=stringdates):
+          if not fieldsadded:
+            fields = record.keys()
+            writer.writerow(fields)
+            fieldsadded = True
+          vals = record.values()
+          writer.writerow(vals)
+          recordCount = recordCount + 1
 
-for dbf in dbfs:
-  recordCount = 0
-  csvfile = re.sub('\.dbf$', '.csv', dbf, flags = re.I)
-  with open(path.join(outputdir, csvfile), 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
-    fieldsadded = False
-    for record in DBF(path.join(dbdir, dbf), char_decode_errors='ignore', parserclass=stringdates):
-      if not fieldsadded:
-        fields = record.keys()
-        writer.writerow(fields)
-        fieldsadded = True
-      vals = record.values()
-      writer.writerow(vals)
-      recordCount = recordCount + 1
-
-  if recordCount == 0:
-    # print('no records in', csvfile)
-    remove(path.join(outputdir, csvfile))
-  else:
-    print(recordCount, 'records transfered from', csvfile)
+      if recordCount == 0:
+        # print('no records in', csvfile)
+        remove(path.join(outputdir, csvfile))
+      else:
+        print(recordCount, 'records transfered from', csvfile)
 
 print('all done')
